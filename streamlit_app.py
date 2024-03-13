05bb04eca86cf118ad2d5b0628e62291eb4b0fe9
He Qi <78305385+HoKei2001@users.noreply.github.com>
2024-03-14 00:10:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,24 @@
 import streamlit as st
-from st_files_connection import FilesConnection
+from streamlit_extras.row import row
+st.set_page_config(
+    page_title="HoKei's page",
+    page_icon="🐸",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
 
-# Create connection object and retrieve file contents.
-# Specify input format is a csv and to cache the result for 600 seconds.
-conn = st.connection('gcs', type=FilesConnection)
-df = conn.read("st-bucket-hokei/myfile.csv", input_format="csv", ttl=600)
+col1, col2 = st.columns([2, 1],gap="large")
+with col1:
+    st.title("👋 Welcome to HoKei's page !")
+    st.info(" ")
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.Owner} has a :{row.Pet}:")
+with col2:
+    st.image('bgpic.png')
+    c1, c2, c3 = st.columns(3)
+    c1.metric("Temperature", "70 °F", "1.2 °F")
+    c2.metric("Wind", "9 mph", "-8%")
+    c3.metric("Humidity", "86%", "4%")
+
+    st.write(": HoKei")
+    st.write("Student")
+    st.info("")
